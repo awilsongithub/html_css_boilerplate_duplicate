ca5f02b3f12b27fcd1a4b390ea1a191e0558f123
Adam Wilson <adamwilson1234@gmail.com>
2015-10-29 16:23:18 -0500

    boilerplate v1 is done with html css readme and js templates

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,13 @@
 // . accesses an attrib. or ability
 window.onload = function() {
   // alert('hey hey hey');
-  statusReport('hey HEY HEY!!!!');
+  statusReport('hey HEY HEY!!!!'); //calls function
+  var username = prompt('What is your name?');
+  var mission = prompt('What is your mission?');
+
+  var contentContainer = document.getElementById('content');
+  contentContainer.innerHTML = username + ' is on a mission to ' + mission;
+
 }
 
 function statusReport(message) {
