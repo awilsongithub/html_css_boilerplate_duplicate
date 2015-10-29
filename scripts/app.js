3e139d8a9a4dcc19ac2e2d6d1d3bb0fc648701b7
Adam Wilson <adamwilson1234@gmail.com>
2015-10-29 16:07:51 -0500

    added window.onload status report console log function to test js and html working together right away.

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+// hey hey hey
+
+
+// . accesses an attrib. or ability
+window.onload = function() {
+  // alert('hey hey hey');
+  statusReport('hey HEY HEY!!!!');
+}
+
+function statusReport(message) {
+  console.log(message);
+  return message;
+}
